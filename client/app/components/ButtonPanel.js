b8a4a844e5d1a51c2382821923a9926e7e96125e
asyasolo <solomkina.ana@yandex.ru>
2023-03-23 17:09:50 +0400

    added button panel and flight list

--- client/app/components/ButtonPanel.js
+++ client/app/components/ButtonPanel.js
@@ -1,7 +1,19 @@
 import React, { useEffect } from "react"
 
 function ButtonPanel() {
-  return <></>
+  return (
+    <div className="button-group">
+      <button className="select-btn">
+        <p>САМЫЙ ДЕШЕВЫЙ</p>
+      </button>
+      <button className="select-btn">
+        <p>САМЫЙ БЫСТРЫЙ</p>
+      </button>
+      <button className="select-btn">
+        <p>ОПТИМАЛЬНЫЙ</p>
+      </button>
+    </div>
+  )
 }
 
 export default ButtonPanel
--- client/app/components/FlightRecord.js
+++ client/app/components/FlightRecord.js
@@ -2,9 +2,29 @@ import React, { useEffect } from "react"
 
 function FlightRecord() {
   return (
-    <>
-      <h1>Cool flight</h1>
-    </>
+    <div className="flight-item">
+      <div className="fligth-item-header">
+        <h1>13 000</h1>
+        <img src="./img/S7_logo.png" alt="" className="flight-logo" />
+      </div>
+
+      <div className="flight-item-body">
+        <div className="flight-item-group">
+          <p className="flight-item-city gray">MOW - HKT</p>
+          <h3 className="flight-item-time">10:45 - 8:00</h3>
+        </div>
+
+        <div className="flight-item-group">
+          <p className="gray">В ПУТИ</p>
+          <h3 className="flight-item-time">10:45 - 8:00</h3>
+        </div>
+
+        <div className="flight-item-group">
+          <p className="gray">2 ПЕРЕСАДКИ</p>
+          <h3 className="flight-item-city-stops">HKG, JNB</h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
