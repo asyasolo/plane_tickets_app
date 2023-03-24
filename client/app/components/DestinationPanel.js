9c616533a01d9882aafc222304fee46ba268c27f
asyasolo <solomkina.ana@yandex.ru>
2023-03-24 15:53:08 +0400

    date picker fix

--- client/app/components/DestinationPanel.js
+++ client/app/components/DestinationPanel.js
@@ -9,8 +9,8 @@ function DestinationPanel() {
       <form className="avia-form">
         <input className="avia-form-input" type="text" placeholder="Откуда" />
         <input className="avia-form-input" type="text" placeholder="Куда" />
-        <input className="avia-form-input" type="text" ref={ref1} onChange={e => console.log(e.target.value)} onFocus={() => (ref1.current.type = "date")} onBlur={() => (ref1.current.type = "date")} placeholder="Туда" />
-        <input className="avia-form-input" type="text" ref={ref2} onChange={e => console.log(e.target.value)} onFocus={() => (ref2.current.type = "date")} onBlur={() => (ref2.current.type = "date")} placeholder="Обратно" />
+        <input className="avia-form-input datepicker-input" type="text" ref={ref1} onChange={e => console.log(e.target.value)} onFocus={() => (ref1.current.type = "date")} onBlur={() => (ref1.current.type = "date")} placeholder="Туда" />
+        <input className="avia-form-input datepicker-input" type="text" ref={ref2} onChange={e => console.log(e.target.value)} onFocus={() => (ref2.current.type = "date")} onBlur={() => (ref2.current.type = "date")} placeholder="Обратно" />
       </form>
     </>
   )
