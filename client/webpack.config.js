0feadec955649c89b7b00188509e22ca4c5cdd79
asyasolo <solomkina.ana@yandex.ru>
2023-03-24 13:15:16 +0400

    modified packages

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./app/Main.js",
   output: {
     publicPath: "/",
-    path: path.resolve(__dirname, "built"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundled.js"
   },
   mode: "development",
