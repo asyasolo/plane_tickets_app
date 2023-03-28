a5b3f3d84ea2fdf9dab73c8091bbea2e9106dc9e
asyasolo <solomkina.ana@yandex.ru>
2023-03-28 16:58:19 +0400

    cadrs filled with info from api

--- client/app/components/ItemList/Item/Item.js
+++ client/app/components/ItemList/Item/Item.js
@@ -1,29 +1,87 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 import "./Item.css"
+import api from "../../../utils/api"
 
 function Item(props) {
+  const [companies, setCompanies] = useState([])
+
+  function convertMsToHours(ms) {
+    const hours = Math.floor(ms / 1000 / 60 / 60)
+    const minutes = Math.floor(ms % 3600000) / 60000
+    return { hours, minutes }
+  }
+
+  function convertUnixTimeToDateTime(unixTime) {
+    const date = new Date(unixTime * 1000)
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const formattedHours = hours < 10 ? `0${hours}` : hours
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes
+    return `${formattedHours}:${formattedMinutes}`
+  }
+
+  function countStops(arr) {
+    switch (arr.length) {
+      case 1:
+        return "Одна пересадка"
+        break
+      case 2:
+        return "Две пересадки"
+        break
+      default:
+        return "Без пересадок"
+        break
+    }
+  }
+
+  function getLogo() {
+    for (let i = 0; i < companies.length; i++) {
+      if (props.companyId == companies[i].id) {
+        return companies[i].logo
+      }
+    }
+    return
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const companiesResponse = await api.fetchCompanies()
+      if (companiesResponse) {
+        setCompanies(companiesResponse)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="flight-item">
       <div className="fligth-item-header">
-        <h1>{props.price}</h1>
-        <img src="./img/S7_logo.png" alt="" className="flight-logo" />
+        <h1>{props.price.toLocaleString("ru-RU")}</h1>
+        <img src={`./img/${getLogo()}`} alt="" className="flight-logo" />
       </div>
 
       <div className="flight-item-body">
         <div className="flight-item-group">
-          <p className="flight-item-city gray">MOW - HKT</p>
-          <h3 className="flight-item-time">10:45 - 8:00</h3>
+          <p className="flight-item-city gray">
+            {props.info.origin} - {props.info.destination}
+          </p>
+          <h3 className="flight-item-time">
+            {convertUnixTimeToDateTime(props.info.dateStart)} - {convertUnixTimeToDateTime(props.info.dateEnd)}
+          </h3>
         </div>
 
         <div className="flight-item-group">
           <p className="gray">В ПУТИ</p>
-          <h3 className="flight-item-time">10:45 - 8:00</h3>
+          <h3 className="flight-item-time">
+            {convertMsToHours(props.info.duration).hours} ч {convertMsToHours(props.info.duration).minutes} мин
+          </h3>
         </div>
 
         <div className="flight-item-group">
-          <p className="gray">2 ПЕРЕСАДКИ</p>
-          <h3 className="flight-item-city-stops">HKG, JNB</h3>
+          <p className="gray">{countStops(props.info.stops)}</p>
+          <h3 className="flight-item-city-stops">{props.info.stops.map(item => item + " ")}</h3>
         </div>
       </div>
     </div>
--- client/app/components/ItemList/ItemList.js
+++ client/app/components/ItemList/ItemList.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react"
 
+import Button from "../misc/Button/Button"
 import Item from "./Item/Item"
 import api from "../../utils/api"
 
 function ItemList() {
-  const [data, setData] = useState([])
+  const [tickets, setTickets] = useState([])
+  const [visibleTickets, setVisibleTickets] = useState(3)
+
+  const handleShowMore = () => {
+    setVisibleTickets(prevVisibleTickets => prevVisibleTickets + 5)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await api.fetchFlights()
-      if (response) {
-        setData(response)
+      const ticketResponse = await api.fetchTickets()
+      if (ticketResponse) {
+        setTickets(ticketResponse)
       }
     }
 
@@ -18,13 +24,13 @@ function ItemList() {
   }, [])
 
   return (
-    <>
-      {data.map(item => {
-        const items = []
-        items.push(<Item key={item.id} info={item.info} companyId={item.companyId} price={item.price} />)
-        return items
-      })}
-    </>
+    <div>
+      {tickets.length === 0 && <div>Loading...</div>}
+      {tickets.slice(0, visibleTickets).map(item => (
+        <Item key={item.id} info={item.info} companyId={item.companyId} price={item.price} />
+      ))}
+      {visibleTickets < tickets.length && <Button onClick={handleShowMore} className="browse_btn" text="ПОКАЗАТЬ ЕЩЕ 5 БИЛЕТОВ" />}
+    </div>
   )
 }
 
