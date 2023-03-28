9fe247b61b23ac5a7b68fe6bead6f5e80e5af858
asyasolo <solomkina.ana@yandex.ru>
2023-03-28 17:12:58 +0400

    added header

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -7,20 +7,24 @@ import ButtonCluster from "./ButtonCluster/ButtonCluster"
 import Form from "./Form/Form"
 import ItemList from "./ItemList/ItemList"
 import HorizontalRule from "./misc/HorizontalRule/HorizontalRule"
+import Header from "./Header/Header"
 
 function App() {
   return (
-    <div className="main">
-      <Form />
-      <HorizontalRule />
-      <div className="main-group">
-        <ExtraOptions />
-        <div className="flight-group">
-          <ButtonCluster />
-          <ItemList />
+    <>
+      <Header />
+      <div className="main">
+        <Form />
+        <HorizontalRule />
+        <div className="main-group">
+          <ExtraOptions />
+          <div className="flight-group">
+            <ButtonCluster />
+            <ItemList />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react"
 
+import "./Header.css"
+
 function Header() {
   return (
     <div className="header-logo">
