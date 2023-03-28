1172d9bf98240b3c781f2ec2b02ac593737a8fab
asyasolo <solomkina.ana@yandex.ru>
2023-03-28 16:56:26 +0400

    Update api.js

--- client/app/utils/api.js
+++ client/app/utils/api.js
@@ -1,11 +1,11 @@
 import axios from "axios"
-/*const cors = require("cors")*/
 
-const apiURL = "https://api.npoint.io/163b5e66df9f2741243e"
+const ticketsURL = "https://api.npoint.io/163b5e66df9f2741243e"
+const companiesURL = "https://api.npoint.io/a1b1c28b32d9785bb26c"
 
-async function fetchFlights() {
+async function fetchTickets() {
   try {
-    const response = await axios.get(`${apiURL}`)
+    const response = await axios.get(`${ticketsURL}`)
     return response.data
   } catch (error) {
     console.error(error)
@@ -13,6 +13,17 @@ async function fetchFlights() {
   }
 }
 
+async function fetchCompanies() {
+  try {
+    const response = await axios.get(`${companiesURL}`)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
 export default {
-  fetchFlights
+  fetchTickets,
+  fetchCompanies
 }
