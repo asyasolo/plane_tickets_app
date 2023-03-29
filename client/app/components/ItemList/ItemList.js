4ce08187824fc355dda56c6024b56d5fc48b172f
asyasolo <solomkina.ana@yandex.ru>
2023-03-29 11:26:32 +0400

    Update ItemList.js

--- client/app/components/ItemList/ItemList.js
+++ client/app/components/ItemList/ItemList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 
-import Button from "../misc/Button/Button"
+import "./ItemList.css"
 import Item from "./Item/Item"
 import api from "../../utils/api"
 
@@ -14,9 +14,13 @@ function ItemList() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const ticketResponse = await api.fetchTickets()
-      if (ticketResponse) {
-        setTickets(ticketResponse)
+      try {
+        const ticketResponse = await api.fetchTickets()
+        if (ticketResponse) {
+          setTickets(ticketResponse)
+        }
+      } catch (error) {
+        console.log(error)
       }
     }
 
@@ -26,10 +30,16 @@ function ItemList() {
   return (
     <div>
       {tickets.length === 0 && <div>Loading...</div>}
-      {tickets.slice(0, visibleTickets).map(item => (
-        <Item key={item.id} info={item.info} companyId={item.companyId} price={item.price} />
-      ))}
-      {visibleTickets < tickets.length && <Button onClick={handleShowMore} className="browse_btn" text="ПОКАЗАТЬ ЕЩЕ 5 БИЛЕТОВ" />}
+      {tickets.slice(0, visibleTickets).map((item, index) => {
+        if (index < visibleTickets) {
+          return <Item key={item.id} info={item.info} companyId={item.companyId} price={item.price} />
+        } else return null
+      })}
+      {visibleTickets < tickets.length && (
+        <button onClick={handleShowMore} className="browse_btn">
+          ПОКАЗАТЬ ЕЩЕ 5 БИЛЕТОВ
+        </button>
+      )}
     </div>
   )
 }
