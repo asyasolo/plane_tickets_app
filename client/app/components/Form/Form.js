f3ea4e4563c727bc6448c9528e55c9fc22b268da
asyasolo <solomkina.ana@yandex.ru>
2023-04-03 13:57:31 +0400

    updated styles

--- client/app/components/Form/Form.js
+++ client/app/components/Form/Form.js
@@ -2,15 +2,15 @@ import React, { useEffect, useRef } from "react"
 
 import "./Form.css"
 
-function Form({ filters, onFilterChange, onSubmit, onResponseChange }) {
+function Form({ filters, onFilterChange, onSubmit }) {
   const ref1 = useRef()
   const ref2 = useRef()
 
   return (
     <>
       <form className="avia-form" onSubmit={onSubmit}>
-        <input className="avia-form-input" type="text" placeholder="Откуда" name="origin" id="origin" value={filters.origin} onChange={onFilterChange} />
-        <input className="avia-form-input" type="text" placeholder="Куда" name="destination" id="destination" value={filters.destination} onChange={onFilterChange} />
+        <input className="avia-form-input spec-placeholder" type="text" placeholder="Откуда" name="origin" id="origin" value={filters.origin} onChange={onFilterChange} />
+        <input className="avia-form-input spec-placeholder" type="text" placeholder="Куда" name="destination" id="destination" value={filters.destination} onChange={onFilterChange} />
         <input className="avia-form-input datepicker-input" type="text" name="dateStart" id="dateStart" value={filters.dateStart} onChange={onFilterChange} ref={ref1} onFocus={() => (ref1.current.type = "date")} onBlur={() => (ref1.current.type = "date")} placeholder="Туда" />
         <input className="avia-form-input datepicker-input" type="text" name="dateEnd" id="dateEnd" value={filters.dateEnd} onChange={onFilterChange} ref={ref2} onFocus={() => (ref2.current.type = "date")} onBlur={() => (ref2.current.type = "date")} placeholder="Обратно" />
         <button className="avia-form-button" type="submit">
--- client/app/components/ItemList/ItemList.js
+++ client/app/components/ItemList/ItemList.js
@@ -6,7 +6,7 @@ import api from "../../utils/api"
 
 function ItemList({ response }) {
   const [tickets, setTickets] = useState([])
-  const [visibleTickets, setVisibleTickets] = useState(3)
+  const [visibleTickets, setVisibleTickets] = useState(2)
 
   const handleShowMore = () => {
     setVisibleTickets(prevVisibleTickets => prevVisibleTickets + 5)
@@ -44,7 +44,31 @@ function ItemList({ response }) {
 
   return (
     <div>
-      {tickets.length === 0 && <div>Loading...</div>}
+      {tickets.length === 0 && (
+        <div className="flight-item">
+          <div className="fligth-item-header">
+            <h1>13 400</h1>
+            <img src={`./img/S7 Logo.png`} alt="" className="flight-logo" />
+          </div>
+
+          <div className="flight-item-body">
+            <div className="flight-item-group">
+              <p className="flight-item-city gray">MOW — HKT</p>
+              <h3 className="flight-item-time">10:45 — 8:00</h3>
+            </div>
+
+            <div className="flight-item-group">
+              <p className="gray">В ПУТИ</p>
+              <h3 className="flight-item-time">21 ч 45 мин</h3>
+            </div>
+
+            <div className="flight-item-group">
+              <p className="gray">Без остановок</p>
+            </div>
+          </div>
+        </div>
+      )}
+
       {tickets.slice(0, visibleTickets).map((item, index) => {
         if (index < visibleTickets) {
           return <Item key={item.id} info={item.info} companyId={item.companyId} price={item.price} />
