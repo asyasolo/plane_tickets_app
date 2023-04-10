529bbf9693612c22232b2311a39226b6ef47f038
asyasolo <solomkina.ana@yandex.ru>
2023-04-10 19:45:55 +0400

    deleted extra files

--- client/app/components/misc/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react"
-
-import "./Button.css"
-
-function Button(props) {
-  return <button className={props.className}>{props.text}</button>
-}
-
-export default Button
