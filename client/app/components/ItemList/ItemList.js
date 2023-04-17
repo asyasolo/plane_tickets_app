341570910b3cc2155b8cdb016b29dcb16e0ff678
asyasolo <solomkina.ana@yandex.ru>
2023-04-17 15:53:31 +0400

    Update ItemList.js

--- client/app/components/ItemList/ItemList.js
+++ client/app/components/ItemList/ItemList.js
@@ -6,6 +6,7 @@ import Item from "./Item/Item"
 function ItemList({ response, sorting }) {
   const [tickets, setTickets] = useState([])
   const [visibleTickets, setVisibleTickets] = useState(3)
+  const [sortedTickets, setSortedTickets] = useState([])
 
   const handleShowMore = () => {
     setVisibleTickets(prevVisibleTickets => prevVisibleTickets + 5)
@@ -13,16 +14,17 @@ function ItemList({ response, sorting }) {
 
   useEffect(() => {
     setTickets(response)
+    setSortedTickets(response)
   }, [response])
 
   useEffect(() => {
     if (sorting === "fastest") {
       const sortedData = [...tickets].sort((a, b) => a.info.duration - b.info.duration)
-      setTickets(sortedData)
+      setSortedTickets(sortedData)
     }
     if (sorting === "cheapest") {
       const sortedData = [...tickets].sort((a, b) => a.price - b.price)
-      setTickets(sortedData)
+      setSortedTickets(sortedData)
     }
     if (sorting === "optimal") {
       const sortedData = [...tickets].sort((a, b) => {
@@ -32,27 +34,26 @@ function ItemList({ response, sorting }) {
           return bRatio - aRatio
         } else return aRatio - bRatio
       })
-      setTickets(sortedData)
+      setSortedTickets(sortedData)
     }
-  }, [sorting])
+  }, [sorting, tickets])
 
   return (
     <div>
       {tickets.length === 0 && <h1>Загрузка</h1>}
 
-      {tickets.length > 0 &&
-        tickets.slice(0, visibleTickets).map((item, index) => {
-          if (index < visibleTickets) {
-            return (
-              <Item
-                key={item.id}
-                info={item.info}
-                companyId={item.companyId}
-                price={item.price}
-              />
-            )
-          } else return null
-        })}
+      {sortedTickets.slice(0, visibleTickets).map((item, index) => {
+        if (index < visibleTickets) {
+          return (
+            <Item
+              key={item.id}
+              info={item.info}
+              companyId={item.companyId}
+              price={item.price}
+            />
+          )
+        } else return null
+      })}
       {visibleTickets < tickets.length && (
         <button
           onClick={handleShowMore}
